ab7ae788d3daa88fc9e65f128fbaad6eaeb0e83e
myeonghwan57 <88016219+myeonghwan57@users.noreply.github.com>
2022-07-21 15:03:56 +0900

    Delete PYTHON/SWEA directory

--- PYTHON/SWEA/swea_2029.py
+++
@@ -1,8 +0,0 @@
-import sys
-
-sys.stdin = open("2029_input.txt", "r")
-
-cnt = int(input())
-for i in range(1,cnt+1):
-    a,b =map(int,input().split())
-    print( "#%d %d %d" % (i, a//b, a%b) )
--- PYTHON/SWEA/swea_2050.py
+++
@@ -1,5 +0,0 @@
-T = input()
-
-for i in T:
-    alph = ord(i)-64
-    print(alph,end=' ')
