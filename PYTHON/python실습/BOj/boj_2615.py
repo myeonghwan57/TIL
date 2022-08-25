103e0989417a7bae822866ca7d20d9fae8c2dc11
myeonghwan57 <myeonghwan57@gmail.com>
2022-08-25 23:12:25 +0900

    UPDATE

--- PYTHON/python실습/BOj/boj_2615.py
+++ PYTHON/python실습/BOj/boj_2615.py
@@ -1,17 +1,39 @@
-N = 19 
-point_list = [list(map(int, input().split())) for _ in range(N)]
-dx = [1, 1, 0, -1]  # 하(↓), 우하(⬊), 우(➞), 우상(⬈)
-dy = [0, 1, 1, 1]
-for x in range(N):
-    for y in range(N):
-        if point_list[x][y] != 0:
+import sys
+input = sys.stdin.readline
+
+board = []
+for i in range(19):
+    board.append(list(map(int, input().split())))
+
+# → ↓ ↘ ↗
+dx = [0, 1, 1, -1]
+dy = [1, 0, 1, 1]
+
+for x in range(19):
+    for y in range(19):
+        if board[x][y] != 0:
+            focus = board[x][y]
+
             for i in range(4):
+                cnt = 1
                 nx = x + dx[i]
                 ny = y + dy[i]
-                cnt = 1
-                if -1 < nx < 19 and -1 < ny < 19:
-                    continue
-                if point_list[x][y] == point_list[nx][ny]:
+
+                while 0 <= nx < 19 and 0 <= ny < 19 and board[nx][ny] == focus:
                     cnt += 1
-                    
-                
+
+                    if cnt == 5:
+                        # 육목 체크
+                        if 0 <= x - dx[i] < 19 and 0 <= y - dy[i] < 19 and board[x - dx[i]][y - dy[i]] == focus:
+                            break
+                        if 0 <= nx + dx[i] < 19 and 0 <= ny + dy[i] < 19 and board[nx + dx[i]][ny + dy[i]] == focus:
+                            break
+                        # 육목이 아니면 성공한거니까 종료
+                        print(focus)
+                        print(x + 1, y + 1)
+                        sys.exit(0)
+
+                    nx += dx[i]
+                    ny += dy[i]
+
+print(0)
--- PYTHON/python실습/BOj/boj_4396.py
+++ PYTHON/python실습/BOj/boj_4396.py
@@ -1,14 +1,28 @@
 n = int(input())
-bomb = []
-game = []
-for _ in range(n):
-    bomb.append(list(input()))
-for _ in range(n):
-    game.append(list(input()))
-result = [[0 for _ in range(n)] for _ in range(n)]
-
+m = 0
+dx = [0, 1, 1, 1, 0, -1, -1, -1]
+dy = [1, 1, 0, -1, -1, -1, 0, 1]
+map = [input() for _ in range(n)]
+check = [input() for _ in range(n)]
+result = ['.' for _ in range(n)]
+flag = 0
 for i in range(n):
     for j in range(n):
-        if bomb[i][j] == '*':
-            if game[i][j] =
-                
+        if check[i][j] == 'x':
+            cnt = 0
+            for d in range(8):
+                nx = i + dx[d]
+                ny = j + dy[d]
+                if 0 < nx < n-1 and 0 < ny < n-1:
+                    map[nx][ny] == '*'
+                    cnt += 1 
+            result[i][j] = str(cnt) 
+            if map[i][j] == '*':
+                flag = 1
+if flag == 1 :
+    for i in range(n):
+        for j in range(n):
+            if map[i][j] == '*':
+                result[i][j] = '*'
+for i in result:
+    print(''.join(i))
