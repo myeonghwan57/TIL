7f9fe782aece494072aaa6fe8f2619db91b80b54
myeonghwan57 <myeonghwan57@gmail.com>
2022-08-30 20:09:20 +0900

    UPDATE

--- PYTHON/python실습/BOj/boj_1371.py
+++ PYTHON/python실습/BOj/boj_1371.py
@@ -1,8 +1,16 @@
 import sys
-str = sys.stdin.read().replace('\n', '').replace(' ','')
-alph = [0]*26
-for i in str:
-    alph[ord(i)-97] += 1
-for i in range(26):
-    if alph[i] == max(alph):
-        print(chr(97+i),end='')
+input = sys.stdin.read
+
+s = input().replace("\n","").replace(" ","")
+c = [0] * 26
+for i in s:
+    c[ord(i)-97]+=1
+    
+maxx = max(c)
+r = []
+for i in range(len(c)):
+    if c[i] == maxx:
+        r.append(chr(i+97))
+        
+r.sort()
+print(*r,sep="")
