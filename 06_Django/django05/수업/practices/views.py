3fac5cc3fc8f681d88a16ae03d9f3a0bef58d50b
myeonghwan57 <myeonghwan57@gmail.com>
2022-09-28 21:39:06 +0900

    django05 수업 practices upload

---
+++ 06_Django/django05/수업/practices/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render
+
+# Create your views here.
+def index(request):
+    context = {}
+    return render(request, "practices/index.html", context)
+    # request, html 파일 이름, context
+
+
+def ping(request):
+    return render(request, "practices/ping.html")
+
+
+def pong(request):
+    # print(request)
+    # print(request.GET.get('ball'))
+    # ball = request.GET.get('ball')
+    # context = {
+    #     'name' : ball, #템플릿 안에서 사용할 것 : 데이터
+    # }
+
+    return render(request, "practices/pong.html", {"name": request.GET.get("ball")})
