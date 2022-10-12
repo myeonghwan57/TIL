6e180b1b4e8e2381b1fc4c4f4e4e978fdc86ad2c
myeonghwan57 <myeonghwan57@gmail.com>
2022-10-12 22:55:31 +0900

    models.py update

--- 06_Django/django10/실습/accounts/models.py
+++ 06_Django/django10/실습/accounts/models.py
@@ -1,5 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+
 # Create your models here.
+# django.contrib.auth.models 의 AbstractUser 상속 받음
+#  User 라는 클래스는 AbstractUser 상속 받음
 class User(AbstractUser):
     pass
