48ad3bda0054cf562b3a6ccdcac1cc1b80255354
myeonghwan57 <myeonghwan57@gmail.com>
2022-10-25 17:20:33 +0900

    articles/views.py update

---
+++ 06_Django/django17/실습/articles/models.py
@@ -0,0 +1,33 @@
+# imagekit 설정
+from imagekit.models import ProcessedImageField
+from imagekit.processors import ResizeToFill
+from django.db import models
+from django.conf import settings
+
+
+# Create your models here.
+# 1. 모델 설계 (DB 스키마 설계)
+class Article(models.Model):
+    title = models.CharField(max_length=20)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    image = ProcessedImageField(
+        upload_to="images/",
+        blank=True,
+        processors=[ResizeToFill(1200, 960)],
+        format="JPEG",
+        options={"quality": 80},
+    )
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    like_users = models.ManyToManyField(
+        settings.AUTH_USER_MODEL, related_name="like_articles"
+    )
+    # 어디에 이미지 업로드 할지 이미지는 항상 업로드하는게 아니기 때문에
+
+
+class Comment(models.Model):
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    article = models.ForeignKey(Article, on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
